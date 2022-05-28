948bbded5f53709703c7052bf56a15af531e3cad
kannna5296 <kannnahacks5296@gmail.com>
2022-05-28 15:44:41 +0900

    lambda微修正

--- aws_lambda.tf
+++ aws_lambda.tf
@@ -1,22 +1,16 @@
 # resource "aws_lambda_function" "my_sample_lambda_function_bytf" {
-#   # If the file is not in the current working directory you will need to include a 
-#   # path.module in the filename.
-#   filename      = "lambda_function_payload.zip"
 #   function_name = "my_sample_lambda_function_bytf"
-#   role          = aws_iam_role.iam_for_lambda.arn
-#   handler       = "index.test"
-
-#   # The filebase64sha256() function is available in Terraform 0.11.12 and later
-#   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-#   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+#   role          = aws_iam_role.iam_for_lambda_bytf.arn
+  
+#   filename      = "app.zip"
 #   source_code_hash = filebase64sha256("lambda_function_payload.zip")
-
-#   runtime = "Python 3.9"
+#   handler       = "app.handler"
+#   runtime = "python3.9"
 # }
 
 
-# resource "aws_iam_role" "iam_for_lambda" {
-#   name = "iam_for_lambda"
+# resource "aws_iam_role" "iam_for_lambda_bytf" {
+#   name = "iam_for_lambda_bytf"
 
 #   assume_role_policy = <<EOF
 # {
