816f2dca2c2566400a7a67067da0a3bfaf6a699e
kannna5296 <kannnahacks5296@gmail.com>
2022-06-25 13:00:31 +0900

    s3生成

--- aws_s3.tf
+++ aws_s3.tf
@@ -1,1 +1,16 @@
-//S3
+resource "aws_s3_bucket" "mysample-bucket-myamane" {
+  bucket = "mysample-bucket-myamane"
+  acl    = "private"
+
+  tags = {
+    Name        = "mysample-bucket-myamane"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.mysample-bucket-myamane.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
