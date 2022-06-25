ad1e2bbe68d95b5a4cfd9c15c619253f6281c956
kannna5296 <kannnahacks5296@gmail.com>
2022-06-25 10:14:55 +0900

    dbいったんコメントアウト（時間かかるので）

--- aws_rds.tf
+++ aws_rds.tf
@@ -1,21 +1,21 @@
-resource "aws_db_instance" "my_sample_db_instance_bytf" {
-    identifier = "mysampledb"
-    allocated_storage = 20
-    engine = "sqlserver-ex"
-    engine_version = "15.00.4198.2.v1"
-    storage_type = "gp2"
-    instance_class = "db.t3.small"
-    availability_zone = "ap-northeast-1a"
-    username = "sa"
-    password = var.sqlsv["password"]
-    port = 1433
-    skip_final_snapshot = true
-    publicly_accessible = true
+# resource "aws_db_instance" "my_sample_db_instance_bytf" {
+#     identifier = "mysampledb"
+#     allocated_storage = 20
+#     engine = "sqlserver-ex"
+#     engine_version = "15.00.4198.2.v1"
+#     storage_type = "gp2"
+#     instance_class = "db.t3.small"
+#     availability_zone = "ap-northeast-1a"
+#     username = "sa"
+#     password = var.sqlsv["password"]
+#     port = 1433
+#     skip_final_snapshot = true
+#     publicly_accessible = true
 
-    db_subnet_group_name = aws_db_subnet_group.my_sample_db_subnet_group_bytf.name
-    parameter_group_name = aws_db_parameter_group.my_sample_db_parameter_group_bytf.name
-    option_group_name = aws_db_option_group.my_sample_db_option_group_bytf.name
-}
+#     db_subnet_group_name = aws_db_subnet_group.my_sample_db_subnet_group_bytf.name
+#     parameter_group_name = aws_db_parameter_group.my_sample_db_parameter_group_bytf.name
+#     option_group_name = aws_db_option_group.my_sample_db_option_group_bytf.name
+# }
 
 resource "aws_db_subnet_group" "my_sample_db_subnet_group_bytf" {
     name = "my_sample_db_subnet_group_bytf"
